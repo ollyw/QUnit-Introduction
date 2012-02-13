b633f2ef6132e628c9e07532e16ac527ece385b3
Oliver Wickham <oliver.wickham@valtech.co.uk>
2012-02-13 14:17:14 +0000

    Cleanup of file names

--- QUnitDemo/QUnitTests/calculator.test.js
+++ QUnitDemo/QUnitTests/calculator.test.js
@@ -1,13 +1,14 @@
 ﻿/// use an xml comment tag to reference javascript file under test
 /// <reference path="/Scripts/MicrosoftAjax.js" />
 /// <reference path="/Scripts/Application/Calculator.js"/>
+/// <reference path="../Scripts/qunit.js" />
 
 (function () {
     "use strict";
     module("Calculator");
 
     test("The calculator must throw an exception if non-numeric characters are entered", 1, function () {
-        QUnit.raises(function() {
+        raises(function() {
             // Arrange
             var input = "invalid input",
                 calculator = new Calculator();
